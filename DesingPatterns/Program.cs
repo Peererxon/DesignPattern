4ff6ba17d8d94c04847c6748f50f5abfde3e7ece
Anderson Gil <andersongil@MacBook-Air-de-Anderson.local>
2022-12-30 08:54:41 -0400

    primer commit

---
+++ DesingPatterns/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace DesingPatterns;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+        var singleton = Singleton.Singleton.Instance;
+    }
+}
+
