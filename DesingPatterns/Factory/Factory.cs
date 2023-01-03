f0ffcb49135ede008ac1f450728141a311f37380
Anderson Gil <andersongil@Laptop-DELL-Anderson.local>
2023-01-03 10:40:14 -0400

    Factory
    Ejemplo en C# de patron de diseño factory
    - Ejemplo con productos y fabricas

---
+++ DesingPatterns/Factory/Factory.cs
@@ -0,0 +1,86 @@
+﻿using System;
+namespace DesingPatterns.Factory
+{
+    // Para cada nuevo producto hay que crear una nueva fabrica porque se asume que el comportamiento
+    // puede ser distinto
+
+
+	//Creator
+	public abstract class SaleFactory
+	{
+		public abstract Isale GetIsale();
+	}
+
+    //Concrete creator
+    public class StoreSaleFactory : SaleFactory
+    {
+        private decimal _extra;
+
+        //El constructor debe ser el unico encargado de recibir estas dependencias
+        // para los objetos
+        public StoreSaleFactory(decimal extra)
+        {
+            _extra = extra;
+        }
+
+        // Metoodo que no recibe parametro
+        public override Isale GetIsale()
+        {
+            return new StoreSale(_extra);
+        }
+    }
+
+    public class InternetSaleFactory : SaleFactory
+    {
+        private decimal _disccount;
+
+        public InternetSaleFactory(decimal disccount)
+        {
+            _disccount = disccount;
+        }
+
+        public override Isale GetIsale()
+        {
+            return new InternetSale(_disccount);
+        }
+    }
+
+    // Concrete product
+    public class StoreSale : Isale
+    {
+        private decimal _extra;
+
+        public StoreSale(decimal extra)
+		{
+			_extra = extra;
+		}
+
+        public void Sell(decimal total)
+        {
+			Console.WriteLine($"La venta en TIENDA tiene un total de {total + _extra}");
+        }
+    }
+
+    public class InternetSale : Isale
+    {
+        private decimal _disccount;
+
+        public InternetSale(decimal disccount)
+        {
+            _disccount = disccount;
+        }
+
+        public void Sell(decimal total)
+        {
+            Console.WriteLine($"La venta en TIENDA tiene un total de {total - _disccount}");
+        }
+    }
+
+    // Product
+    public interface Isale
+	{
+		public void Sell(decimal total);
+	}
+
+}
+
--- DesingPatterns/Program.cs
+++ DesingPatterns/Program.cs
@@ -1,14 +1,25 @@
-﻿namespace DesingPatterns;
+﻿using DesingPatterns.Factory;
+
+namespace DesingPatterns;
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
-        var singleton = Singleton.Singleton.Instance;
-        var singleton2 = Singleton.Singleton.Instance;
-        var log = Singleton.Log.Instance;
-        log.Save("a");
-        log.Save("hellou!");
+        //Console.WriteLine("Hello, World!");
+        //var singleton = Singleton.Singleton.Instance;
+        //var singleton2 = Singleton.Singleton.Instance;
+        //var log = Singleton.Log.Instance;
+        //log.Save("a");
+        //log.Save("hellou!");
+
+        SaleFactory storeSaleFactory = new StoreSaleFactory(10);
+        SaleFactory internetSaleFactory = new InternetSaleFactory(2);
+
+        Isale sale1 = storeSaleFactory.GetIsale();
+        sale1.Sell(15);
+
+        Isale sale2 = internetSaleFactory.GetIsale();
+        sale2.Sell(12);
     }
 }
 
