9354258fba4efe7238d825342bbee454fdfa476f
andersongil <agilp@copaair.com>
2023-03-04 22:57:44 -0400

    primeros pasos para epository, accediendo al context y cambiando claves

---
+++ DesingPatterns/Models/DesignPatternsContext.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+
+namespace DesingPatterns.Models;
+
+public partial class DesignPatternsContext : DbContext
+{
+    public DesignPatternsContext()
+    {
+    }
+
+    public DesignPatternsContext(DbContextOptions<DesignPatternsContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Beer> Beers { get; set; }
+
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+#warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
+        => optionsBuilder.UseSqlServer("Server=localhost; Database=DesignPatterns; User Id=sa;Password=Anderson2.; TrustServerCertificate=True; ");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder.Entity<Beer>(entity =>
+        {
+            entity.HasKey(e => e.BeerId).HasName("PK__Beer__293C94BF04CFA952");
+
+            entity.ToTable("Beer");
+
+            entity.Property(e => e.BeerId).ValueGeneratedNever();
+            entity.Property(e => e.Name).HasMaxLength(50);
+            entity.Property(e => e.Style).HasMaxLength(50);
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
--- DesingPatterns/Program.cs
+++ DesingPatterns/Program.cs
@@ -1,4 +1,5 @@
 ﻿using DesingPatterns.Factory;
+using DesingPatterns.Models;
 
 namespace DesingPatterns;
 class Program
@@ -11,6 +12,19 @@ class Program
         //var log = Singleton.Log.Instance;
         //log.Save("a");
         //log.Save("hellou!");
+
+        using (var context = new DesignPatternsContext())
+        {
+            var lts = context.Beers.ToList();
+            Console.WriteLine("I am iron man",lts);
+
+            Console.WriteLine("HOsLA");
+
+            foreach (var beer in lts) {
+                Console.WriteLine(beer.Name);
+            }
+            Console.ReadKey();
+        }
 
         SaleFactory storeSaleFactory = new StoreSaleFactory(10);
         SaleFactory internetSaleFactory = new InternetSaleFactory(2);
