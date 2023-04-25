7dbe99c9bac0b13ef78a114ea5cc03c3f58e785d
Anderson Gil <andersongil.dev@gmail.com>
2023-04-25 13:08:28 -0400

    ejemplo de builder preparando un objeto mas complejo

---
+++ Tools/Generator/Generator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.Json;
+using System.Threading.Tasks;
+
+namespace Tools.Generator
+{
+    public class Generator
+    {
+        //el contenido que se guarda va aca en el objeto
+
+        public List<string> Content { get; set; }
+        public string Path { get; set; }
+        public TypeFormat Format { get; set; }
+
+        public TypeCharacter Character { get; set; }
+
+        public void Save()
+        {
+            string result = "";
+            result = Format == TypeFormat.Json ? GetJson() : GetPipe();
+
+            if(Character == TypeCharacter.Uppercase)
+            {
+                result = result.ToUpper();
+            } else if ( Character == TypeCharacter.Lowercase)
+            {
+                result = result.ToLower();
+            }
+
+            File.WriteAllText(Path,result);
+        }
+
+        private string GetJson() => JsonSerializer.Serialize(Content);
+
+        private string GetPipe() => Content.Aggregate((accum,current) => accum + "|" + current );
+    }
+}
---
+++ Tools/Generator/GeneratorDirector.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tools.Generator
+{
+    public class GeneratorDirector
+    {
+
+        private IBuilderGenerator _generatorBuilder;
+
+        GeneratorDirector(IBuilderGenerator generatorBuilder)
+        {
+            SetBuilder(generatorBuilder);
+        }
+
+        public void SetBuilder(IBuilderGenerator generatorBuilder) => _generatorBuilder = generatorBuilder;
+
+        public void CreateSimpleJsonGenerator(List<string> content, string path)
+        {
+            _generatorBuilder.Reset();
+            _generatorBuilder.SetContent(content);
+            _generatorBuilder.SetPath(path);
+            _generatorBuilder.SetFormat(TypeFormat.Json);
+        }
+
+        public void CreateSimplePipeGenerator(List<string> content, string path)
+        {
+            _generatorBuilder.Reset();
+            _generatorBuilder.SetContent(content);
+            _generatorBuilder.SetPath(path);
+            _generatorBuilder.SetFormat(TypeFormat.Pipes);
+            _generatorBuilder.SetCharacter(TypeCharacter.Uppercase);
+        }
+
+    }
+}
